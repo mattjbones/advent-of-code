bec46ed7f768f636ecb231bb123b5c471d8622f7
mattjbones <mattjbones@gmail.com>
2022-12-08 17:50:27 +0000

    part 2

--- src/day/eight/mod.rs
+++ src/day/eight/mod.rs
@@ -72,46 +72,9 @@ fn calculate_visibility_grid(
                 .iter()
                 .any(|tree| test <= *tree);
 
-            // print!("target: {test}, row {row}, col {index}\t");
-            // println!(
-            //     "rl {}, rr {}, cl {}, cr {}",
-            //     visible_in_row_left,
-            //     visible_in_row_right,
-            //     visible_in_col_left,
-            //     visible_in_col_right
-            // );
-
-            // if row == 3 && index == 1 {
-            //     println!("{:#?}, {:#?}", left_col, right_col.split_first().unwrap().1);
-            //     // println!("{:#?}, {:#?}", left_row, right_row.split_first().unwrap().1);
-
-            //     panic!("ded")
-            // }
-
             let visible_in_row = visible_in_row_left || visible_in_row_right;
             let visible_in_col = visible_in_col_left || visible_in_col_right;
-            // let visible_in_row = tree_grid_rows[row]
-            //     .iter()
-            //     .enumerate()
-            //     .all(|(i, tree)| test >= *tree);
-
-            // let visible_in_col = tree_grid_cols[index]
-            //     .iter()
-            //     .enumerate()
-            //     .all(|(i, tree)| test >= *tree);
-
-            // println!("column {:#?}", visible_in_col);
-            // println!("visible {:#?}", visible_in_row);
-            // println!("visible from outside {}", visible_in_col && visible_in_row);
             vis_grid[row][index] = visible_in_col || visible_in_row;
-            // println!(
-            //     "test {test} is col: {}, row: {}",
-            //     visible_in_col, visible_in_row
-            // );
-            // panic!(
-            //     "cols {:#?}, rows {:#?}",
-            //     tree_grid_cols[index], tree_grid_rows[row]
-            // );
         }
         row += 1;
     }
@@ -119,8 +82,69 @@ fn calculate_visibility_grid(
     vis_grid
 }
 
-fn calcualte_visibility_score(tree_grid_cols: Grid, tree_grid_rows: Grid) -> i32 {
-    0
+fn calculate_visibility_score_for_trees(trees: &[usize], target: usize) -> i32 {
+    if trees.len() == 0 {
+        0
+    } else if trees.len() == 1 {
+        1
+    } else {
+        let mut score = 0;
+        for tree in trees {
+            score += 1;
+            if *tree >= target {
+                break;
+            }
+        }
+        score
+    }
+}
+
+fn calculate_visibility_score(tree_grid_cols: Grid, tree_grid_rows: Grid) -> i32 {
+    let width = tree_grid_rows.first().unwrap().len();
+    let height = tree_grid_rows.len();
+    let mut score_grid = vec![vec![0; width]; height];
+
+    let mut row = 0;
+    while row < score_grid.len() {
+        let vis_grid_row = &score_grid[row];
+        for index in 0..vis_grid_row.len() {
+            let test = tree_grid_rows[row][index];
+
+            let (left_row, right_row) = tree_grid_rows[row].split_at(index);
+            let mut reversed_left_row = left_row.to_vec();
+            reversed_left_row.reverse();
+            let left_row_score = calculate_visibility_score_for_trees(&reversed_left_row, test);
+            let right_row_score =
+                calculate_visibility_score_for_trees(right_row.split_first().unwrap().1, test);
+
+            let (left_col, right_col) = tree_grid_cols[index].split_at(row);
+            let mut reversed_left_col = left_col.to_vec();
+            reversed_left_col.reverse();
+            let left_col_score = calculate_visibility_score_for_trees(&reversed_left_col, test);
+            let right_col_score =
+                calculate_visibility_score_for_trees(right_col.split_first().unwrap().1, test);
+
+            let score = left_col_score * left_row_score * right_col_score * right_row_score;
+            // if row == 1 && index == 2 {
+            //     println!("target {test}");
+            //     println!(
+            //         "left {}, right {}, up {}, down {}",
+            //         left_row_score, right_row_score, left_col_score, right_col_score
+            //     );
+            //     println!("{:?} {:?}", left_row, right_row.split_first().unwrap().1);
+            //     println!("{:?} {:?}", left_col, right_col.split_first().unwrap().1);
+            //     panic!("lol");
+            // }
+            score_grid[row][index] = score
+        }
+        row += 1;
+    }
+
+    let mut flat_grid = score_grid.iter().flatten().copied().collect::<Vec<i32>>();
+    // println!("flat {flat_grid:?}");
+    flat_grid.sort();
+    // println!("sorted {flat_grid:?}");
+    *flat_grid.last().unwrap()
 }
 
 pub fn run() {
@@ -139,12 +163,12 @@ pub fn run() {
     }
 
     {
-        println!("Part 2");
-        let tree_grid_rows = generate_tree_rows_from_input_string(include_str!("sample"));
+        println!("\nPart 2");
+        let tree_grid_rows = generate_tree_rows_from_input_string(include_str!("input"));
         let tree_grid_cols = generate_tree_cols_from_rows(&tree_grid_rows);
-        let scenic_score = calcualte_visibility_score(tree_grid_cols, tree_grid_rows);
+        let scenic_score = calculate_visibility_score(tree_grid_cols, tree_grid_rows);
 
         println!("Score: {scenic_score}");
-        println!("Expected: 8")
+        println!("Expected: 327180")
     }
 }
