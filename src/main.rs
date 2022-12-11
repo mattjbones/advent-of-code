7f9a4269290711915f470f6458e35474de7d10af
mattjbones <mattjbones@gmail.com>
2022-12-11 17:39:19 +0000

    order, order

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![feature(iter_next_chunk)]
 
-use std::{collections::HashMap, env::args, process::exit};
+use std::{env::args, process::exit};
 
 pub mod day;
 
@@ -8,18 +8,18 @@ type DayRunner = fn();
 fn main() {
     println!("Advent of code");
 
-    let days: HashMap<String, DayRunner> = HashMap::from([
-        ("1".to_string(), day::one::run as fn()),
-        ("2".to_string(), day::two::run),
-        ("3".to_string(), day::three::run),
-        ("4".to_string(), day::four::run),
-        ("5".to_string(), day::five::run),
-        ("6".to_string(), day::six::run),
-        ("7".to_string(), day::seven::run),
-        ("8".to_string(), day::eight::run),
-        ("9".to_string(), day::nine::run),
-        ("10".to_string(), day::ten::run),
-        ("11".to_string(), day::eleven::run),
+    let days: Vec<DayRunner> = Vec::from([
+        day::one::run as fn(),
+        day::two::run,
+        day::three::run,
+        day::four::run,
+        day::five::run,
+        day::six::run,
+        day::seven::run,
+        day::eight::run,
+        day::nine::run,
+        day::ten::run,
+        day::eleven::run,
     ]);
 
     let args = args().into_iter().collect::<Vec<String>>();
@@ -32,9 +32,16 @@ fn main() {
 
     let selected = args[1].as_str();
     if selected == "all" {
-        days.into_iter().for_each(|(_, day)| day());
-    } else if let Some(day) = days.get(selected) {
-        day();
+        days.into_iter().for_each(|day| day());
+    } else if let Ok(selected_no) = selected.parse::<usize>() {
+        if selected_no > days.len() {
+            println!("Day not valid");
+
+            println!("Select day no or \"all\"");
+            println!("e.g. cargo run -- 9");
+            exit(0);
+        }
+        days[selected_no - 1]();
     } else {
         println!("Select day no or \"all\"");
         println!("e.g. cargo run -- 9");
