70f3e3fc87d32153d96a8fcf24f87cb87c050b3b
mattjbones <mattjbones@gmail.com>
2023-12-06 18:42:10 +0000

    refactors to try for part 2

--- 2023-cpp/src/days/five/five.cpp
+++ 2023-cpp/src/days/five/five.cpp
@@ -1,8 +1,9 @@
-
-#include <algorithm>
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/split.hpp>
+
+#include <algorithm>
 #include <climits>
+#include <cstdlib>
 #include <iostream>
 #include <iterator>
 #include <string>
@@ -61,13 +62,16 @@ struct Almanac {
     std::vector<MapCategory> categories;
 };
 
+struct Range {
+    string name;
+    long start = 0;
+    long end = 0;
+};
+
 struct MapRange {
     string name;
-    long dest_start = 0;
-    long dest_end = 0;
-    long source_start = 0;
-    long source_end = 0;
-    long diff = 0;
+    std::vector<Range> source;
+    std::vector<Range> dest;
 };
 
 void print_entry(MapEntry& entry)
@@ -90,45 +94,26 @@ void print_category(MapCategory& cat, int display_count = 0)
     });
 }
 
-void print_range(MapRange& range)
-{
-    print_line("name: " + range.name);
-    print_line("source_start: " + to_string(range.source_start));
-    print_line("source_end: " + to_string(range.source_end));
-    print_line("dest_start: " + to_string(range.dest_start));
-    print_line("dest_end: " + to_string(range.dest_end));
-}
+// void print_range(MapRange& range)
+// {
+//     print_line("name: " + range.name);
+//     print_line("source_start: " + to_string(range.source_start));
+//     print_line("source_end: " + to_string(range.source_end));
+//     print_line("dest_start: " + to_string(range.dest_start));
+//     print_line("dest_end: " + to_string(range.dest_end));
+// }
 
 void print_ranges(std::vector<MapRange>& ranges)
 {
     int count = 0;
     std::for_each(ranges.begin(), ranges.end(), [&count](MapRange& range) {
         print_line("(" + to_string(count) + ")");
-        print_range(range);
+        // print_range(range);
         print_line("");
         ++count;
     });
 
     print_line("");
-}
-
-void print_range_grid(std::vector<std::vector<MapRange>>& ranges)
-{
-    auto range = ranges.front();
-
-    std::vector<long> source;
-    std::vector<long> dest;
-    for (auto& range : range) {
-        print_range(range);
-        source.push_back(range.source_start);
-        source.push_back(range.source_end);
-
-        dest.push_back(range.dest_start);
-        dest.push_back(range.dest_end);
-    }
-
-    print_lines(&source);
-    print_lines(&dest);
 }
 
 Almanac parse_input_for_seeds(problem_lines ptr_lines)
@@ -190,76 +175,76 @@ Almanac parse_input_for_almanac(problem_lines ptr_lines)
     return almanac;
 }
 
-std::vector<vector<MapRange>> generated_map_ranges_from_almanac(Almanac& almanac)
+std::vector<MapRange> generated_map_ranges_from_almanac(Almanac& almanac)
 {
-    std::vector<vector<MapRange>> ranges;
+    std::vector<MapRange> ranges;
     std::for_each(almanac.categories.begin(), almanac.categories.end(), [&ranges](MapCategory& category) mutable {
         // print_category(category);
-        std::vector<MapRange> category_ranges;
-        std::for_each(
-            category.entries.begin(), category.entries.end(), [&category_ranges, &category](MapEntry& entry) mutable {
+        std::vector<Range> source_ranges;
+        std::vector<Range> dest_ranges;
+
+        std::for_each(category.entries.begin(), category.entries.end(),
+            [&dest_ranges, &source_ranges, &category](MapEntry& entry) mutable {
                 // print_entry(entry);
 
-                MapRange category_range;
-                category_range.name = category.name;
-                category_range.dest_start = entry.dest_start;
-                category_range.dest_end = entry.dest_start + entry.length - 1;
-                category_range.source_start = entry.source_start;
-                category_range.source_end = entry.source_start + entry.length - 1;
-                category_range.diff = entry.dest_start - entry.source_start;
+                source_ranges.push_back(Range {
+                    category.name,
+                    entry.source_start,
+                    entry.source_start + entry.length - 1,
+                });
 
-                category_ranges.push_back(category_range);
+                dest_ranges.push_back(Range {
+                    category.name,
+                    entry.dest_start,
+                    entry.dest_start + entry.length - 1,
+                });
                 // print_ranges(category_ranges);
             });
 
-        ranges.push_back(category_ranges);
+        ranges.push_back(MapRange { category.name, source_ranges, dest_ranges });
     });
 
     return ranges;
 }
 
-long calculate_seed_location(long& seed_location, std::vector<vector<MapRange>>& ranges)
+long calculate_seed_location(long& seed_location, std::vector<MapRange>& ranges)
 {
     // print_line("Checking seed: " + to_string(seed_location));
     long source = seed_location;
     long destination = 0;
     cout << "Seed " + to_string(seed_location) + ", ";
-    std::for_each(
-        ranges.begin(), ranges.end(), [&ranges, &source, &destination](std::vector<MapRange>& mapRange) mutable {
-            // print_line("Checking range: " + mapRange.front().name);
-            // print_line("source: " + to_string(source));
-            bool found_matching_range = false;
-            std::for_each(mapRange.begin(), mapRange.end(),
-                [&destination, &source, &found_matching_range](MapRange& range) mutable {
-                    // print_line("Range: ");
-                    // print_range(range);
-                    if (!found_matching_range) {
-                        if (range.source_start <= source && source <= range.source_end) {
-                            found_matching_range = true;
-                            // print_line("Found ma");
-                            destination = source + range.diff;
-                            // do some maths;
-                        }
-                    }
-                    // print_line("");
-                });
-            if (!found_matching_range) {
-                destination = source;
+    std::for_each(ranges.begin(), ranges.end(), [&ranges, &source, &destination](MapRange& mapRange) mutable {
+        bool found_matching_range = false;
+        for (int i = 0; i < mapRange.source.size(); i++) {
+            auto range = mapRange.source[i];
+            auto dest_range = mapRange.dest[i];
+            if (range.start <= source && source <= range.end) {
+                found_matching_range = true;
+
+                auto diff = dest_range.start - range.start;
+
+                destination = source + diff;
+                // do some maths;
+                break;
             }
-            cout << mapRange.front().name + " ";
-            cout << destination;
-            cout << ", ";
-            // print_line("Seed: " + to_string(source) + " maps to soil: " + to_string(destination));
-            source = destination;
-            // exit(1);
-        });
+        }
+        if (!found_matching_range) {
+            destination = source;
+        }
+        cout << mapRange.name + " ";
+        cout << destination;
+        cout << ", ";
+        // print_line("Seed: " + to_string(source) + " maps to soil: " + to_string(destination));
+        source = destination;
+        // exit(1);
+    });
 
     cout << endl;
     // print_line("Source: " + to_string(source) + " Destination: " + to_string(destination));
     return destination;
 }
 
-long calculate_lowest_seed_location_from_ranges(std::vector<long>& seeds, std::vector<vector<MapRange>>& ranges)
+long calculate_lowest_seed_location_from_ranges(std::vector<long>& seeds, std::vector<MapRange>& ranges)
 {
     long lowest = LONG_MAX;
 
@@ -290,36 +275,49 @@ int Five::get_part_1_result(problem_lines ptr_lines)
     return (int)location;
 }
 
-struct SeedRange {
-    long start = 0;
-    long end = 0;
-};
-
-std::vector<SeedRange> generate_seeds_from_almanac(std::vector<long>& seed_ranges)
+std::vector<Range> generate_seeds_from_almanac(std::vector<long>& seed_ranges)
 {
-    std::vector<SeedRange> ranges;
+    std::vector<Range> ranges;
     for (int i = 0; i < seed_ranges.size(); i += 2) {
 
         const long start = seed_ranges[i];
         const long length = seed_ranges[i + 1];
 
-        const SeedRange seed_range({ start, start + length - 1 });
+        const Range seed_range({ "Seed", start, start + length - 1 });
 
         ranges.push_back(seed_range);
     }
 
-    std::for_each(ranges.begin(), ranges.end(), [](SeedRange& range) {
-        print_line("Start: " + to_string(range.start));
-        print_line("End: " + to_string(range.end));
-    });
-
     return ranges;
 }
 
-long calculate_lowest_seed_location_from_ranges(std::vector<SeedRange>& seeds, std::vector<vector<MapRange>>& ranges)
+long calculate_seed_location_from_seed_range(Range& seed_range, std::vector<MapRange>& ranges)
 {
+    // print_line("Seed: " + to_string(seed_range.start) + ", " + to_string(seed_range.end));
+    // auto first = ranges.front();
+    // for(Range &source_ranges : first.source){
 
-    print_range_grid(ranges);
+    // }
+
+    //     for (int i = 0; i < first.source.size(); i++) {
+    //         auto range = first.source[i];
+    //         auto dest_range = first.dest[i];
+    //         if (range.start <= source && source <= range.end) {
+
+    //             auto diff = dest_range.start - range.start;
+
+    //             destination = source + diff;
+    //             // do some maths;
+    //             break;
+    //         }
+    //     }
+
+    return 0;
+}
+
+long calculate_lowest_seed_location_from_ranges(std::vector<Range>& seeds, std::vector<MapRange>& ranges)
+{
+    auto res = calculate_seed_location_from_seed_range(seeds.front(), ranges);
     return 0;
 };
 
--- 2023-cpp/src/logging.tcc
+++ 2023-cpp/src/logging.tcc
@@ -5,12 +5,28 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <boost/container/flat_set.hpp>
 
 using namespace std;
 
 template <typename T> void print_error(T line) { cerr << line << endl; }
 template <typename T> void print_line(T line) { cout << line << endl; }
 template <typename T> void print_lines(vector<T>* lines, bool samelines = false)
+{
+    std::for_each(lines->begin(), lines->end(), [&samelines](T line) {
+        if (!samelines) {
+            print_line(line);
+        } else {
+            cout << line;
+            cout << " ";
+        }
+    });
+    if (samelines) {
+        cout << endl;
+    }
+}
+
+template <typename T> void print_lines(boost::container::flat_set<T>* lines, bool samelines = false)
 {
     std::for_each(lines->begin(), lines->end(), [&samelines](T line) {
         if (!samelines) {
