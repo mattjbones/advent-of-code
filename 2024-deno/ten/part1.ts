6064b2ebc55b65d49de23f3dc9b3c8e1c816989b
mattjbones <mattjbones@gmail.com>
2025-01-03 15:51:45 +0200

    day 10 part 1

---
+++ 2024-deno/ten/part1.ts
@@ -0,0 +1,118 @@
+import { Matrix } from "@izure/coord-matrix2d";
+
+// const map = Deno.readTextFileSync("./example1"); //2
+// const map = Deno.readTextFileSync("./example2"); // 4
+// const map = Deno.readTextFileSync("./example3"); // 3
+// const map = Deno.readTextFileSync("./sample"); // 36
+const map = Deno.readTextFileSync("./input");
+
+const lines = map.split("\n");
+const width = lines[0].length;
+const height = lines.length;
+
+const grid = Matrix.Create(
+  width,
+  height,
+  lines.flatMap((line) => line.split("").map((val) => parseInt(val, 10))),
+);
+
+function printGrid<T extends number | string>(grid: Matrix<T>) {
+  console.log(
+    grid.as2DArray().map((row) =>
+      row.map((val) => typeof val === "string" ? val : isNaN(val) ? "." : val)
+        .join(" ")
+    ).join("\n"),
+  );
+}
+
+enum Direction {
+  Left,
+  Right,
+  Up,
+  Down,
+}
+function getElementPositionForDirection(
+  grid: Matrix<number>,
+  position: [number, number],
+  direction: Direction,
+): [[number, number], number] {
+  switch (direction) {
+    case Direction.Left: {
+      const nextLeft: [number, number] = [position[0], position[1] + 1];
+      if (nextLeft[1] > grid.row) {
+        return [position, grid.getElement(...position)];
+      }
+      const nextLeftElement = grid.getElement(...nextLeft);
+      return [nextLeft, nextLeftElement];
+    }
+    case Direction.Right: {
+      const nextRight: [number, number] = [position[0], position[1] - 1];
+      if (nextRight[1] < 1) return [position, grid.getElement(...position)];
+      const nextRightElement = grid.getElement(...nextRight);
+      return [nextRight, nextRightElement];
+    }
+    case Direction.Up: {
+      const nextUp: [number, number] = [position[0] + 1, position[1]];
+      if (nextUp[0] > grid.col) {
+        return [position, grid.getElement(...position)];
+      }
+      const nextUpElement = grid.getElement(...nextUp);
+      return [nextUp, nextUpElement];
+    }
+    case Direction.Down: {
+      const nextUp: [number, number] = [position[0] - 1, position[1]];
+      if (nextUp[0] < 1) {
+        return [position, grid.getElement(...position)];
+      }
+      const nextUpElement = grid.getElement(...nextUp);
+      return [nextUp, nextUpElement];
+    }
+  }
+}
+
+printGrid(grid);
+
+const directions = [
+  Direction.Up,
+  Direction.Left,
+  Direction.Right,
+  Direction.Down,
+];
+// find starting positions
+const starts: [number, number][] = [];
+for (let r = 1; r < width + 1; r++) {
+  for (let c = 1; c < height + 1; c++) {
+    const element = grid.getElement(r, c);
+    if (element === 0) starts.push([r, c]);
+  }
+}
+
+// DFS
+let endCount = 0;
+while (starts.length) {
+  const visited: string[] = [];
+  const queue: [number, number][] = [];
+  queue.push(starts.pop()!);
+  while (queue.length) {
+    const position = queue.pop();
+    if (!position) break;
+    const element = grid.getElement(...position);
+    if (element === 9) endCount++;
+    visited.push(position.join(","));
+    directions.forEach((direction) => {
+      const [nextPosition, nextElement] = getElementPositionForDirection(
+        grid,
+        position,
+        direction,
+      );
+      if (
+        !isNaN(nextElement) && nextElement - element === 1 &&
+        !visited.includes(nextPosition.join(","))
+      ) {
+        queue.push(nextPosition);
+      }
+    });
+  }
+}
+
+console.log({ endCount });
